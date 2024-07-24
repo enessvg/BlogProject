d2606547987b418b20200cc83611a53a0baf4cb1
Enes Sevga <enessvg4@gmail.com>
2024-07-24 09:53:52 +0300

    Update 0001_01_01_000000_create_users_table.php

--- blog-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ blog-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             // $table->string('is_admin')->default('0');
-            $table->string('profile_photo')->nullable();
+            $table->string('avatar_url')->nullable();
             $table->string('profile_bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
